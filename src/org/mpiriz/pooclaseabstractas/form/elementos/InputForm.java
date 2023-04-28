fb68a33c72945e869f9f17e5baf36919dcd3cb8b
Matias Ezequiel <matiaspirizcocio98@gmail.com>
2023-04-28 18:03:14 +0200

    1-he creado la clase ElementoForm, Input y TextareaForm, con sus atributos, constructores, setters y métodos.

---
+++ src/org/mpiriz/pooclaseabstractas/form/elementos/InputForm.java
@@ -0,0 +1,32 @@
+package org.mpiriz.pooclaseabstractas.form.elementos;
+
+public class InputForm extends ElementoForm {
+    // si heremos heredar de una clase que es abstracta tendremos que implementar sus methods
+
+
+    private String tipo;
+
+
+    public InputForm(String tipo){
+        this.tipo = tipo;
+    }
+
+    public InputForm(String nombre, String tipo){
+        super(nombre);
+        this.tipo = tipo;
+    }
+
+    public String getTipo(){
+        return tipo;
+    }
+
+    public void setTipo(String tipo){
+        this.tipo = tipo;
+    }
+    @Override
+    public String dibujarHtml() {
+        return "<input type=\"" + this.tipo
+                + "\" name=\"" + this.nombre +
+                "\" value=\"" + this.valor + "\" >";
+    }
+}
---
+++ src/org/mpiriz/pooclaseabstractas/form/elementos/TextareaForm.java
@@ -0,0 +1,10 @@
+package org.mpiriz.pooclaseabstractas.form.elementos;
+
+public class TextareaForm extends ElementoForm{
+
+
+    @Override
+    public String dibujarHtml() {
+        return null;
+    }
+}
